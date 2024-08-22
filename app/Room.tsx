6020db14f6d23869ecac59925f7b516e73f588db
The-Ankit-Maurya <ankitmaurya20223@gmail.com>
2024-08-22 19:23:02 +0530

    Figma Project Initiated

---
+++ app/Room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_T8sa2o0rhsUDBmf2GSwE5p_XVjD350N5uAdLWkcpl59P_JTJDWMTqH5U2LPVZqOb"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
